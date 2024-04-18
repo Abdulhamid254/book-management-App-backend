b72906dc2ed093b8d681ce92455382de27dc0764
Abdulhamid254 <nassurabdulhamid49@gmail.com>
2024-04-18 16:41:13 +0300

    initialized the application and prepared the infrastructure with docker compose

---
+++ book-mgnt/src/main/java/com/abdul/bookmgnt/BookMgntApiApplication.java
@@ -0,0 +1,13 @@
+package com.abdul.bookmgnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMgntApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMgntApiApplication.class, args);
+	}
+
+}
---
+++ book-mgnt/src/test/java/com/abdul/bookmgnt/BookMgntApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdul.bookmgnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMgntApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
