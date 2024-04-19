ea4d43ac05eadde4375ae5af133163ae39e76f90
Abdulhamid254 <nassurabdulhamid49@gmail.com>
2024-04-19 09:34:50 +0300

    Created the user entity

--- book-mgnt/src/main/java/com/abdul/bookmgnt/BookMgntApiApplication.java
+++ book-mgnt/src/main/java/com/abdul/bookmgnt/BookMgntApiApplication.java
@@ -2,8 +2,10 @@ package com.abdul.bookmgnt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BookMgntApiApplication {
 
 	public static void main(String[] args) {
